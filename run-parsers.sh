4d1ca0f3890f649384de430497f73a2a7f63a8ac
Nicolas Bigot <nicbigot@gmail.com>
2016-11-01 14:00:46 +0100

    script has been renamed

--- run-parsers.sh
+++ run-parsers.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 source /home/rss/rssenv/bin/activate
-python spider_rss_v2.py -w "developpez"
-python spider_rss_v2.py -w "lesechos"
-python spider_rss_v2.py -w "polygon"
+python spider_rss.py -w "developpez"
+python spider_rss.py -w "lesechos"
+python spider_rss.py -w "polygon"
 deactivate
